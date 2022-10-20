b53fa470f33e60d380d82f7af9089155ec01f790
Rifqikelazzz04 <100737539+Rifqiyudo@users.noreply.github.com>
2022-10-20 23:41:53 +0700

    Add files via upload

---
+++ No4 C.php
@@ -0,0 +1,16 @@
+<!--- Nama    : Rifqi yudo dewantoro
+      NIM     : 21091397074
+      Kelas   : 2021 b--->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
---
+++ interface no 3.php
@@ -0,0 +1,15 @@
+<!-- Rifqi Yudo Dewantoro
+     21091397074 -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
